0c5d34af4cae2f8c1c67dc0c1a1277038f1bddda
Kaktuzo4ek <slobodian.vit2103@gmail.com>
2023-05-04 11:37:36 +0300

    change DB CS

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import multer from "multer";
@@ -12,7 +14,7 @@ import {
 import { handleValidationErrors, checkAuth } from "./utils/index.js";
 import { UserController, PostController } from "./controllers/index.js";
 
-await mongoose
+mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("DB OK!");
@@ -41,6 +43,12 @@ app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "API WORKING",
+  });
+});
+
 app.post(
   "/auth/login",
   loginValidation,
@@ -58,12 +66,6 @@ app.get("/auth/me", checkAuth, UserController.getMe);
 app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
   res.json({
     url: `uploads/${req.file.originalname}`,
-  });
-});
-
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "API WORKING",
   });
 });
 
@@ -92,6 +94,5 @@ app.listen(process.env.PORT || 4444, (err) => {
   if (err) {
     return console.log(err);
   }
-
   console.log("Server OK!");
 });
